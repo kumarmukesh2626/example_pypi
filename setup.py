44a7d11f67d9157f2fa2938d38fa13bc14b31f1f
kumarmukesh2626 <mukesh.kumar@algo8.ai>
2023-03-02 16:34:08 +0530

    changes insetup file

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "algo8_template"
+REPO_NAME = "example_pypi"
 AUTHOR_USER_NAME = "kumarmukesh2626"
-SRC_REPO = "algo8_template"
+SRC_REPO = "example_pypi"
 AUTHOR_EMAIL = "mukesh.kumar@algo8.ai"
 
 setuptools.setup(
